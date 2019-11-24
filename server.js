637aeb46df5fc4444a11807b04ae2ffc76b0923e
poonamsingh1993 <poonams.ps34@gmail.com>
2019-11-24 23:07:53 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var app = express();
+var jsforce = require('./src/lib/JSForceConnection');
+var bodyParser = require('body-parser');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+
+// app.use(cors())
+app.use(cookieParser());
+// set the view engine to ejs
+//app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+// set the home page route
+app.get('/', function (req, res) {
+	// ejs render automatically looks in the views folder
+	res.sendFile(path.join(__dirname + '/public/Html/Index.htm'));
+});
+
+app.get('/oauth2/auth', function (req, res) {
+	console.log('parms' + req.query.env);
+	jsforce.redirectToAuthentication(res, req.query.env);
+});
+
+
+app.get('/oauth2/auth/callBack_success', function (req, res) {
+	console.log('token' + req.query.code);
+	console.log(res)
+	jsforce.authenticateConnection(req.query.code).
+		then((userInfo) => {
+			console.log('Authentication Successful' + JSON.stringify(userInfo));
+			res.cookie('token', req.query.code).sendFile(path.join(__dirname + '/public/Html/package.html'));
+		}).
+		catch((err) => {
+			console.log('error' + err);
+		});
+});
+
+app.listen(port, function () {
+	console.log('Our app is running on http://localhost:' + port);
+});
+
+
